279b35c72766d549fba1c4d5a01c1d51f31d9372
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-29 17:52:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,8 +4,8 @@ def join_nested_strings(src)
   strings_from_src_arrays = []
   for i in 0...src.length
     for f in 0...src[i].length
-      puts src[i][f].class.class
-      if src[i][f].class == 'String'
+      puts src[i][f].class
+      if src[i][f].class == String
         strings_from_src_arrays.push(src[i][f])
         puts src[i][f]
       end
